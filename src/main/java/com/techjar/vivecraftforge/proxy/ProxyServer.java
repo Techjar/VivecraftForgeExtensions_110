cd350c266b056ea7090a6636d7efb85891a98ec2
Techjar <tecknojar@gmail.com>
2017-05-16 02:36:54 -0400

    Don't register climbey recipes if disabled

--- src/main/java/com/techjar/vivecraftforge/proxy/ProxyServer.java
+++ src/main/java/com/techjar/vivecraftforge/proxy/ProxyServer.java
@@ -1,5 +1,6 @@
 package com.techjar.vivecraftforge.proxy;
 
+import com.techjar.vivecraftforge.Config;
 import com.techjar.vivecraftforge.eventhandler.EventHandlerServer;
 import com.techjar.vivecraftforge.network.ChannelHandler;
 
@@ -28,13 +29,15 @@ public class ProxyServer extends ProxyCommon {
 	@Override
 	public void registerRecipes() {
 		super.registerRecipes();
-		ItemStack jumpBoots = new ItemStack(Items.LEATHER_BOOTS).setStackDisplayName("Jump Boots");
-		jumpBoots.getTagCompound().setBoolean("Unbreakable", true);
-		jumpBoots.getTagCompound().setInteger("HideFlags", 4);
-		GameRegistry.addRecipe(jumpBoots, "B", "S", 'S', Blocks.SLIME_BLOCK, 'B', Items.LEATHER_BOOTS);
-		ItemStack climbClaws = new ItemStack(Items.SHEARS).setStackDisplayName("Climb Claws");
-		climbClaws.getTagCompound().setBoolean("Unbreakable", true);
-		climbClaws.getTagCompound().setInteger("HideFlags", 4);
-		GameRegistry.addRecipe(climbClaws, "S S", "C C", 'S', Items.SPIDER_EYE, 'C', Items.SHEARS);
+		if (Config.climbeyEnabled) {
+			ItemStack jumpBoots = new ItemStack(Items.LEATHER_BOOTS).setStackDisplayName("Jump Boots");
+			jumpBoots.getTagCompound().setBoolean("Unbreakable", true);
+			jumpBoots.getTagCompound().setInteger("HideFlags", 4);
+			GameRegistry.addShapedRecipe(jumpBoots, "B", "S", 'S', Blocks.SLIME_BLOCK, 'B', Items.LEATHER_BOOTS);
+			ItemStack climbClaws = new ItemStack(Items.SHEARS).setStackDisplayName("Climb Claws");
+			climbClaws.getTagCompound().setBoolean("Unbreakable", true);
+			climbClaws.getTagCompound().setInteger("HideFlags", 4);
+			GameRegistry.addShapedRecipe(climbClaws, "S S", "C C", 'S', Items.SPIDER_EYE, 'C', Items.SHEARS);
+		}
 	}
 }
