f8bad806754ecea03698489cd214953e1248b8b6
Techjar <tecknojar@gmail.com>
2017-08-15 14:30:11 -0400

    Fix obf names for 1.12.1

--- src/main/java/com/techjar/vivecraftforge/core/asm/ObfNames.java
+++ src/main/java/com/techjar/vivecraftforge/core/asm/ObfNames.java
@@ -1,15 +1,15 @@
 package com.techjar.vivecraftforge.core.asm;
 
 public class ObfNames {
-	public static final String ENTITYAICREEPERSWELL = "yh";
-	public static final String ENTITYLIVINGBASE = "vn";
-	public static final String ENTITYENDERMAN = "acs";
-	public static final String ENTITYPLAYER = "aeb";
-	public static final String VEC3D = "bnc";
-	public static final String NETHANDLERPLAYSERVER = "oy";
+	public static final String ENTITYAICREEPERSWELL = "yj";
+	public static final String ENTITYLIVINGBASE = "vp";
+	public static final String ENTITYENDERMAN = "acu";
+	public static final String ENTITYPLAYER = "aed";
+	public static final String VEC3D = "bne";
+	public static final String NETHANDLERPLAYSERVER = "pa";
 	public static final String CPACKETPLAYER = "lk";
-	public static final String CPACKETPLAYERTRYUSEITEMONBLOCK = "lz";
-	public static final String CPACKETPLAYERDIGGING = "lo";
+	public static final String CPACKETPLAYERTRYUSEITEMONBLOCK = "ma";
+	public static final String CPACKETPLAYERDIGGING = "lp";
 	public static final String CPACKETUSEENTITY = "li";
-	public static final String ENTITYPLAYERMP = "oo";
+	public static final String ENTITYPLAYERMP = "oq";
 }
